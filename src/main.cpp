85e3a613ca384969c109fefb6350f3bfff2288db
Dieter Blocher <didi@netjam.de>
2025-07-11 14:58:58 +0200

    Add first project

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+// Interne LED Pin (auf dem D1 Mini ist das GPIO2/D4)
+#define LED_BUILTIN 2
+
+void setup() {
+  // Serielle Kommunikation starten
+  Serial.begin(115200);
+  
+  // LED Pin als Output konfigurieren
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Warten bis Serial bereit ist
+  delay(1000);
+  
+  Serial.println();
+  Serial.println("Hello World from ESP8266 Mini D1!");
+  Serial.println("LED Blink Demo gestartet...");
+}
+
+void loop() {
+  // LED einschalten (LOW = an, da die interne LED invertiert ist)
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED AN");
+  delay(1000);
+  
+  // LED ausschalten (HIGH = aus)
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED AUS");
+  delay(1000);
+}
